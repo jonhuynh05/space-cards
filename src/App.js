f730d555913671076879c7751011ab2e52f6d0c6
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-27 11:58:50 -0800

    mount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   const [test, setTest] = useState(false)
-  const getData = async () => {
-    const data = await fetch("/api/v1")
-    console.log(data)
-  }
+  // const getData = async () => {
+  //   const data = await fetch("/api/v1")
+  //   console.log(data)
+  // }
+  
+  useEffect(() => {
+    console.log("this mounted")
+  })
+
+  
   return (
     <div className="App">
       <header className="App-header">
         hello
       </header>
-      <button onClick={getData}>Data</button>
+      {/* <button onClick={getData}>Data</button> */}
     </div>
   );
 }
