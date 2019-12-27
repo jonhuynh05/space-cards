eb6539c858c337b20417d899c627c246cecc463f
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-27 13:12:02 -0800

    add description text

--- src/App.js
+++ src/App.js
@@ -15,10 +15,16 @@ function App() {
 
   
   return (
-    <div className="App">
-      <header className="App-header">
-        hello
-      </header>
+    <div className="container">
+        <div className="header">
+            Get Ready To Learn About Space!
+        </div>
+        <div className="description">
+            CosmoCards is here to help you learn about all things space. Pick one of the categories below to start going through flashcards!
+        </div>
+        <div>
+            Select A Category
+        </div>
     </div>
   );
 }
