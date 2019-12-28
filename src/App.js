74ff6092b09e10aac03a02fa5c5d2f3dea4b73b5
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-28 11:02:43 -0800

    center flashcard icons

--- src/App.js
+++ src/App.js
@@ -22,9 +22,29 @@ function App() {
         <div className="description">
             CosmoCards is here to help you learn about all things space. Pick one of the categories below to start going through flashcards!
         </div>
-        <div>
+        <div className="select">
             Select A Category
         </div>
+            <div className="category-container">
+                <div className="category">
+                    Category Name 1
+                </div>
+                <div className="category">
+                    Category Name 2
+                </div>
+                <div className="category">
+                    Category Name 3
+                </div>
+                <div className="category">
+                    Category Name 4
+                </div>
+                <div className="category">
+                    Category Name 5
+                </div>
+                <div className="category">
+                    Category Name 6
+                </div>
+          </div>
     </div>
   );
 }
