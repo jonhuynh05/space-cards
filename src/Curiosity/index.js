7a4b719ca89d5faef3aeadddeac64a7e817665a6
Jonathan Huynh <jonhuynh05@gmail.com>
2020-01-03 14:45:13 -0800

    add style to images

--- src/Curiosity/index.js
+++ src/Curiosity/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import {MoonLoader} from "react-spinners"
+import "./curiosity.css"
 
 function Curiosity() {
     const [isLoading, setIsLoading] = useState(true)
@@ -26,23 +27,23 @@ function Curiosity() {
 
     const displayImages = images.map((photo, i) => {
         return (
-            <div key={i}>
-                <img src={photo}/>
+            <div key={i} className="images-row">
+                <img className="curiosity-image" id={`curiosity-${i}`} src={photo}/>
             </div>
         )
     })
 
     return(
-        <div className="container">
+        <div className="container" id="curiosity">
             this is curiosity
             <MoonLoader
                 size={"50px"}
                 color={"rgb(138, 138, 255)"}
                 loading={isLoading}
             />
-            <>
+            <div className="container" id="images-container">
                 {displayImages}
-            </>
+            </div>
         </div>
     )
 }
